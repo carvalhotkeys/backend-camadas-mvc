b4fd01e7bb24a9fc2b0c00d02ae82410c74e1730
carvalhotkeys <vendascarvalhoti@gmail.com>
2023-03-19 13:33:54 -0300

    remove

--- index.js
+++
@@ -1,7 +0,0 @@
-import Cliente from './modelo/Cliente.js';
-
-let usuario = new Cliente('Cristiano Carvalho', '32154785420', '18995542200' ,'Rua João Formosa 500', 'Centro', 'Rosana', 'SP', 'cristiano3112@gmail.com');
-
-usuario.nome = 'CARVALHO CRISTIANO' // vc pode alterar o nome endereco telefne email, so troca na frente do usuario
-
-console.log(usuario.toJSON());
